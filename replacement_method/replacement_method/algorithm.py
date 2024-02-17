16b8655baccbbe322d5136fd8a205c28d62c24a5
coolworld2049 <coolworld2049@gmail.com>
2024-02-18 01:25:51 +0300

    polybius_square

--- replacement_method/replacement_method/algorithm.py
+++ replacement_method/replacement_method/algorithm.py
@@ -1,18 +1,27 @@
+import string
+from pprint import pprint
+
+
 def generate_trisemus_table(keyword):
-    keyword = keyword.lower()
-    unique_keyword = "".join(sorted(set(keyword), key=keyword.index))
-
     alphabet = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя"
-    table = [list(unique_keyword)]
+    keyword = keyword.lower().replace(" ", "")
+    unique_keyword = "".join(sorted(set(keyword), key=keyword.index))
+    remaining_chars = "".join(sorted(set(alphabet) - set(unique_keyword)))
 
-    for letter in alphabet:
-        if letter not in unique_keyword:
-            table[0].append(letter)
+    # same_chars = [['е', 'ё'], ['и', 'й'], ['ш', 'щ'], ['ь', 'ъ']]
+    # for i, orig in enumerate(remaining_chars):
+    #     for same_char in same_chars:
+    #         if orig in same_char:
+    #             for char_replacement in same_char:
+    #                 remaining_chars = remaining_chars.replace(char_replacement, "")
 
-    for row in range(1, len(alphabet)):
-        table.append(table[0][row:] + table[0][:row])
+    polybius_square_string = unique_keyword + remaining_chars + string.punctuation
+    polybius_square = [
+        list(polybius_square_string[i : i + 6])
+        for i in range(0, len(polybius_square_string), 6)
+    ]
 
-    return table
+    return polybius_square
 
 
 def trisemus_encrypt(plaintext, table):
@@ -20,13 +29,18 @@ def trisemus_encrypt(plaintext, table):
     ciphertext = ""
     for char in plaintext:
         if char == " ":
-            ciphertext += " "
-        else:
-            for row in range(len(table)):
-                if char in table[row]:
-                    col = table[row].index(char)
-                    ciphertext += table[(row + 1) % len(table)][col]
-                    break
+            ciphertext += char
+            continue
+        for row in range(len(table)):
+            if char in table[row]:
+                col = table[row].index(char)
+                replace_row = (row + 1) % len(table)
+                cipher_value = table[replace_row][col]
+                ciphertext += cipher_value
+                print(
+                    f"'{char}' -> '{cipher_value}'; row: {row}, col: {col}, replace_row: {replace_row}"
+                )
+                break
 
     return ciphertext
 
@@ -48,13 +62,18 @@ def trisemus_decrypt(ciphertext, table):
 
 
 # Пример использования
-keyword = "ключ"
-plaintext = "методы криптографической защиты"
+keyword = "секреты"
+plaintext = (
+    "Я в своем познании настолько преисполнился, что я как будто бы уже сто триллионов миллиардов"
+    " лет проживаю на триллионах и триллионах таких же планет, как эта Земля, мне этот мир"
+    " абсолютно понятен, и я здесь ищу только одного - покоя, умиротворения"
+)
 
 table = generate_trisemus_table(keyword)
 encrypted_text = trisemus_encrypt(plaintext, table)
 decrypted_text = trisemus_decrypt(encrypted_text, table)
 
-print("Original Text:", plaintext)
-print("Encrypted Text:", encrypted_text)
-print("Decrypted Text:", decrypted_text)
+print(f"table:")
+pprint(table)
+print(f"\nEncrypted Text:\n{encrypted_text}")
+print(f"\nDecrypted Text:\n{decrypted_text}")
